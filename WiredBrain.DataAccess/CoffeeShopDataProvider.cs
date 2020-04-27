7c9157411296c7ebff86dff92a6f9ace51f3ac18
pwbolton77 <pwbolton77@gmail.com>
2020-04-27 07:20:28 -0700

    Added paper cup data to provider

--- WiredBrain.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrain.DataAccess/CoffeeShopDataProvider.cs
@@ -7,9 +7,9 @@ namespace WiredBrain.DataAccess
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops()
         {
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
-            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107, PaperCupsInStock = 20 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23, PaperCupsInStock = 30 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56, PaperCupsInStock = 40 };
         }
     }
 }
